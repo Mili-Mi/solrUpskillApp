06cf8f77348b66d39c59f3b6847e1e73e459d8a7
Mili-Mi <70621334+Mili-Mi@users.noreply.github.com>
2021-10-16 23:09:43 +0530

    added product item

--- src/app/model/product.model.ts
+++ src/app/model/product.model.ts
@@ -6,5 +6,14 @@ public pquantity:number;
 public pprice:number;
 public pcategory:string;
 public imagepath:string;
-
+constructor( pid:number,pname:string,pdescription:string,pquantity:number, pprice:number,
+ pcategory:string,imagepath:string){
+    this.pid =pid;
+    this.pname =pname;
+    this.pdescription=pdescription;
+    this.pquantity=pquantity;
+    this.pprice=pprice;
+    this.pcategory=pcategory;
+    this.imagepath=imagepath;
+}
 }
--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,3 +1,4 @@
+import { RecursiveTemplateAstVisitor } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/model/product.model';
 
@@ -7,7 +8,10 @@ import { Product } from 'src/app/model/product.model';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-product:Product[] =[];
+product:Product[] =[
+  new Product(1,'iphone','New iphone launched with great features',2,102000,'mobile',
+  'https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fiphone-12&psig=AOvVaw0W3JlThkNxGCYHRy-5Vokx&ust=1634491399832000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOCyxo25z_MCFQAAAAAdAAAAABAD')
+];
 
   constructor() { }
 
