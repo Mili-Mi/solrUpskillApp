576ef0caa25fb263197433c3c486d3db3bba3d93
Mili-Mi <70621334+Mili-Mi@users.noreply.github.com>
2021-10-17 14:16:32 +0530

    added image

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -8,9 +8,9 @@ import { Product } from 'src/app/model/product.model';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-product:Product[] =[
+products:Product[] =[
   new Product(1,'iphone','New iphone launched with great features',2,102000,'mobile',
-  'https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fiphone-12&psig=AOvVaw0W3JlThkNxGCYHRy-5Vokx&ust=1634491399832000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOCyxo25z_MCFQAAAAAdAAAAABAD')
+  'https://www.reliancedigital.in/medias/Apple-12-Smartphones-491901533-i-1-1200Wx1200H?context=bWFzdGVyfGltYWdlc3wxMTMwMTd8aW1hZ2UvanBlZ3xpbWFnZXMvaDM2L2g1OC85NDA3NzMxMTcxMzU4LmpwZ3w5NjBiYTIzZWE1Yjg5NjQzN2YyZTAxZjNhNGI2ODg0YzQ4NmZlMDZiN2EwYmVkYjlhZjA3OGIxNDZiNDEzNTc0')
 ];
 
   constructor() { }
